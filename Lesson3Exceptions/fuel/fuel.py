989702ff91ea2e6c4577cd7394dd86142fd16ba8
Seamus Connolly <sghconnolly@gmail.com>
2024-10-20 16:07:13 +0100

    Added the solution code

--- Lesson3Exceptions/fuel/fuel.py
+++ Lesson3Exceptions/fuel/fuel.py
@@ -4,3 +4,35 @@ In a file called fuel.py, implement a program that prompts the user for a fracti
 If, though, X or Y is not an integer, X is greater than Y, or Y is 0, instead prompt the user again. (It is not necessary for Y to be 4.) Be sure to catch any exceptions like ValueError or ZeroDivisionError.
 """
 
+def main():
+    # Use the get_input function to return the fuel percentage
+    print(get_input())
+
+
+# get_input checks for exceptions and calls calc_percentage to define levels.
+def get_input():
+    while True:
+        try:
+            x, y = input("Enter fraction of fuel: ").strip().split("/")
+            x = int(x)
+            y = int(y)
+            percent = calc_percentage(round(x / y * 100))
+        except ValueError:
+            print("Value Error!")
+        except ZeroDivisionError:
+            print("Zero division error!")
+        else:
+            if x <= y:  # Checks that the user has not input a value greater than 100%
+                return percent
+
+
+# calc_percentafe checks for the correct level indication
+def calc_percentage(level):
+    if level >= 99:
+        return "F"
+    elif level <= 1:
+        return "E"
+    else:
+        return str(level) + "%"
+
+main()
