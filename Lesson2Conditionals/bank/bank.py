10a42c8b8cb454a23050ea08ddf5ff7275b69d74
Seamus Connolly <sghconnolly@gmail.com>
2024-10-20 10:22:19 +0100

    Code for the exercise using if elif and else

--- Lesson2Conditionals/bank/bank.py
+++ Lesson2Conditionals/bank/bank.py
@@ -1,2 +1,13 @@
 # In a file called bank.py, implement a program that prompts the user for a greeting. If the greeting starts with “hello”, output $0. If the greeting starts with an “h” (but not “hello”), output $20. Otherwise, output $100. Ignore any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
 
+def main():
+    greeting = input("Greeting: ").lower().strip()
+
+    if greeting[0:5] == "hello":
+        print("$0")
+    elif greeting[0] == "h":
+        print("$20")
+    else:
+        print("$100")
+
+main()
