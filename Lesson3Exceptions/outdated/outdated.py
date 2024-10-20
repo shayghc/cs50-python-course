ac8f388e25d3d9c0f067ea64f576b38b83a7ee53
Seamus Connolly <sghconnolly@gmail.com>
2024-10-20 16:05:48 +0100

    Add a comment with an explanation of the exercise

---
+++ Lesson3Exceptions/outdated/outdated.py
@@ -0,0 +1,20 @@
+"""
+In a file called outdated.py, implement a program that prompts the user for a date, anno Domini, in month-day-year order, formatted like 9/8/1636 or September 8, 1636, wherein the month in the latter might be any of the values in the list below:
+
+[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+Then output that same date in YYYY-MM-DD format. If the user’s input is not a valid date in either format, prompt the user again. Assume that every month has no more than 31 days; no need to validate whether a month has 28, 29, 30, or 31 days.
+"""
+
