22b9a7f5a85b5d7cbec04112abda5b401a92eb66
Seamus Connolly <sghconnolly@gmail.com>
2024-10-20 19:03:08 +0100

    Spelling

--- Lesson4Libraries/emojize/emojize.py
+++ Lesson4Libraries/emojize/emojize.py
@@ -8,7 +8,6 @@ def main():
     user_code = input("Enter an emoji code: ")
     # The language parameter allows aliases to be recognised
     converted_to_emoji = emoji.emojize(user_code, language='alias')
-
     print("Output: ", converted_to_emoji)
     
 if __name__ == "__main()__":
