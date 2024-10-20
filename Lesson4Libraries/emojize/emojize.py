3e50dc6ebf25fa46bc1ebd22bcba1a47a5407a06
Seamus Connolly <sghconnolly@gmail.com>
2024-10-20 18:57:37 +0100

    Code solution added

---
+++ Lesson4Libraries/emojize/emojize.py
@@ -0,0 +1,15 @@
+import emoji
+"""
+In a file called emojize.py, implement a program that prompts the user for a str in English and then outputs the “emojized” version of that str, converting any codes (or aliases) therein to their corresponding emoji.
+"""
+
+  
+def main():
+    user_code = input("Enter an emoji code: ")
+    # The language parameter allows aliases to be recognised
+    converted_to_emoji = emoji.emojize(user_code, language='alias')
+
+    print("Output: ", converted_to_emoji)
+    
+if __name__ == "__main()__":
+    main()
