2d84824f53198694d9f2170f66cd6c6b0994715d
Seamus Connolly <sghconnolly@gmail.com>
2024-10-20 10:30:18 +0100

    Code for exercise

--- Lesson2Conditionals/extensions/extensions.py
+++ Lesson2Conditionals/extensions/extensions.py
@@ -10,3 +10,24 @@ In a file called extensions.py, implement a program that prompts the user for th
 .zip
 If the file’s name ends with some other suffix or has no suffix at all, output application/octet-stream instead, which is a common default.
 """
+def main():
+    name = input("File name: ").strip().lower()
+
+    if name.endswith(".gif"):
+        print("image/gif")
+    elif name.endswith(".jpg"):
+        print("image/jpeg")
+    elif name.endswith(".jpeg"):
+        print("image/jpeg")
+    elif name.endswith(".png"):
+        print("image/png")
+    elif name.endswith(".pdf"):
+        print("application/pdf")
+    elif name.endswith(".txt"):
+        print("text/plain")
+    elif name.endswith(".zip"):
+        print("application/zip")
+    else:
+        print("application/octet-stream")
+
+main()
