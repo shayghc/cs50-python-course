fc4298cc2232d75a5ef0fd6d6040840ed2102fff
Seamus Connolly <sghconnolly@gmail.com>
2024-10-20 16:43:33 +0100

    Format change

--- Lesson3Exceptions/taqueria/taqueria.py
+++ Lesson3Exceptions/taqueria/taqueria.py
@@ -1,5 +1,5 @@
 """
-In a file called taqueria.py, implement a program that enables a user to place an order, prompting them for items, one per line, until the user inputs control-d (which is a common way of ending one’s input to a program). After each inputted item, display the total cost of all items inputted thus far, prefixed with a dollar sign ($) and formatted to two decimal places. Treat the user’s input case insensitively. Ignore any input that isn’t an item. Assume that every item on the menu will be titlecased.
+In a file called taqueria.py, implement a program that enables a user to place an order, prompting them for items, one per line, until the user inputs control-d (which is a common way of ending one's input to a program). After each inputted item, display the total cost of all items inputted thus far, prefixed with a dollar sign ($) and formatted to two decimal places. Treat the user's input case insensitively. Ignore any input that isn't an item. Assume that every item on the menu will be title-cased.
 """
 
 d = {
