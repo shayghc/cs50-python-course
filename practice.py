822923fbb252df13678e5c28b5f1e615784ae736
Seamus Connolly <sghconnolly@gmail.com>
2024-10-29 11:15:32 +0000

    Changes for file IO practice

--- practice.py
+++ practice.py
@@ -1,5 +1,9 @@
-import cowsay
-import sys
+names = []
 
-if len(sys.argv) == 2:
-    cowsay.trex("Hello, " + sys.argv[1])
+with open("names.txt", "r") as file:
+    for line in file:
+        names.append(line.strip())
+        
+with open("names.txt", "w") as file:
+    for name in sorted(names):
+        file.write(f"{name}\n")
