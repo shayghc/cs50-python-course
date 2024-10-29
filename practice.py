b3bb5f4ccdbc3f3a282cf0528301fa117abc0dd5
Seamus Connolly <sghconnolly@gmail.com>
2024-10-29 13:09:52 +0000

    Code practice for file IO

--- practice.py
+++
@@ -1,9 +0,0 @@
-names = []
-
-with open("names.txt", "r") as file:
-    for line in file:
-        names.append(line.strip())
-        
-with open("names.txt", "w") as file:
-    for name in sorted(names):
-        file.write(f"{name}\n")
