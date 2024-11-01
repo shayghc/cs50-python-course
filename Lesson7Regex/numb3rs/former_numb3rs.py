3a0cc2b174f5c13e3cae7b261253a8238d9a050c
Seamus Connolly <sghconnolly@gmail.com>
2024-11-01 06:33:45 +0000

    Docs

--- Lesson7Regex/numb3rs/former_numb3rs.py
+++ Lesson7Regex/numb3rs/former_numb3rs.py
@@ -2,10 +2,33 @@ import re
 
 
 def main():
+    """
+    This function is the entry point of the program. It prompts the user to input an IPv4 address,
+    validates it using the validate function, and prints the result.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
     print(validate(input("IPv4 Address: ")))
 
 
 def validate(ip):
+        """
+    Validates an IPv4 address.
+
+    This function takes a string representing an IPv4 address as input and checks if it is a valid IPv4 address.
+    It uses regular expressions to match the input against the pattern of an IPv4 address. If the input matches the pattern,
+    it further checks if each octet of the address is within the valid range of 0 to 255.
+
+    Parameters:
+    ip (str): A string representing an IPv4 address.
+
+    Returns:
+    bool: True if the input is a valid IPv4 address, False otherwise.
+    """
     if matches := re.search(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", ip):
         try:
             octet1 = int(matches.group(1))
--- Lesson7Regex/numb3rs/test_numb3rs.py
+++ Lesson7Regex/numb3rs/test_numb3rs.py
@@ -4,21 +4,25 @@ from numb3rs import validate
 def test_empty_string():
     assert validate("") == False
 
+
 def test_whitespace_removed():
     assert validate("  255.100.99.5") == False
     assert validate("  255.100.99.5") == False
     assert validate("255.100.99.5") == True
+
 
 def test_delimiter():
     assert validate("123,123,123,123") == False
     assert validate("123 123 123 123") == False
     assert validate("123.123.123.123") == True
 
+
 def test_non_alpha():
     assert validate("cat.123.123.123") == False
     assert validate("123.cat.123.123") == False
     assert validate("123.123.cat.123") == False
     assert validate("123.123.123.cat") == False
+
 
 def test_octet_range():
     assert validate("256.0.0.0") == False
@@ -30,12 +34,25 @@ def test_octet_range():
     assert validate("0.0.-1.0") == False
     assert validate("0.0.0.-1") == False
 
+
 def test_four_digits():
     assert validate("1234.1.1.1") == False
     assert validate("1.1234.1.1") == False
     assert validate("1.1.1234.1") == False
     assert validate("1.1.1.1234") == False
 
+
 def test_four_octets():
     assert validate("1.2.3.4.5") == False
     assert validate("1.2.3") == False
+    
+
+""" CS50's  check 50 doe snot like testing for leading zeros
+It exits with exit(1) if the IP address starts with a zero
+
+def test_leading zeros():
+    assert validate("0.0.0.1") == False
+    assert validate("0.0.1.0") == False
+    assert validate("0.1.0.0") == False
+    assert validate("1.0.0.0") == False
+"""
