72d06d0e2a5c40cdbb1f90423f7103733422a3ce
Seamus Connolly <sghconnolly@gmail.com>
2024-11-02 07:41:56 +0000

    Short regex pattern for test

--- Lesson7Regex/watch/watch.py
+++ Lesson7Regex/watch/watch.py
@@ -7,10 +7,10 @@ def main():
 
 
 def parse(s):
-    ...
-
-
-...
+    if match := re.search(r"youtube.com\/embed\/(\w)+" ,s)
+        return f"https://youtu.be/{match.group(1)}"
+    else:
+        return None
 
 
 if __name__ == "__main__":
